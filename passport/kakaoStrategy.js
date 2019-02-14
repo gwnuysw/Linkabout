e2d04cd919bf2bcb87a06af842444a6ec339e3de
seokwon <swlee1014@gmail.com>
2019-02-14 21:06:32 +0900

    checkout to react

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -13,6 +13,7 @@ module.exports = (passport) => {
         done(null, exUser);
       }
       else{
+        console.log('email find', profile._json, profile._json.kaccout_email);
         const newUser = await user.create({
           email: profile._json && profile._json.kaccount_email,
           nick: profile.displayName,
